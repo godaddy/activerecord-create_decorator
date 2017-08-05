b6b6b2173352ec2f7af015eb0907f0d484e0d660
Michael Tucker <mtucker@godaddy.com>
2017-08-05 07:24:36 -0700

    replace alias_method_chain approach

--- lib/activerecord/schema_statements_ext.rb
+++ lib/activerecord/schema_statements_ext.rb
@@ -1,25 +1,26 @@
-ActiveRecord.module_eval do
-  ActiveRecord::ConnectionAdapters.module_eval do
-    ActiveRecord::ConnectionAdapters::SchemaStatements.module_eval do
-      def create_table_with_options_decoration(table_name, options={}, &block)
-        create_options = ActiveRecord::Base.connection_config[:create_options]
-        if create_options
-          current_options = options[:options]
-          if current_options
-            options[:options] = "#{current_options} #{create_options}"
-          else
-            options[:options] = create_options
-          end
-        end
-
-        if block_given?
-          create_table_without_options_decoration(table_name, options, &block)
-        else
-          create_table_without_options_decoration(table_name, options)
-        end
+module ActiveRecord::ConnectionAdapters::SchemaStatementsExt
+  def create_table(table_name, options={}, &block)
+    create_options = ActiveRecord::Base.connection_config[:create_options]
+    if create_options
+      current_options = options[:options]
+      if current_options
+        options[:options] = "#{current_options} #{create_options}"
+      else
+        options[:options] = create_options
       end
-
-      alias_method_chain :create_table, :options_decoration
     end
+
+    super
   end
 end
+
+# bundle modified behavior with the SchemaStatements module
+ActiveRecord::ConnectionAdapters::SchemaStatements.module_eval do
+
+  # when SchemaStatements is included, add SchemaStatementsExt overrides
+  def self.included(mod)
+    mod.include ActiveRecord::ConnectionAdapters::SchemaStatementsExt 
+  end
+
+end
+
