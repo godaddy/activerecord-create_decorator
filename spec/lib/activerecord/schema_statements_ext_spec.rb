ac728b96d8c62b8e4a12cb65a7317398e0916f10
jakebrady5 <jakebrady5@gmail.com>
2017-08-24 21:22:59 -0700

    pass specs for AR >= 4

--- spec/lib/activerecord/schema_statements_ext_spec.rb
+++ spec/lib/activerecord/schema_statements_ext_spec.rb
@@ -16,14 +16,16 @@ RSpec.describe ActiveRecord::ConnectionAdapters::SchemaStatementsExt do
   end
 
   before(:each) do
-    expect_any_instance_of(TestClass).to receive_message_chain(:schema_creation, :accept)
-    expect_any_instance_of(TestClass).to receive(:execute)
-    expect_any_instance_of(TestClass).to receive(:supports_indexes_in_create?)
-    expect_any_instance_of(TestClass).to receive(:supports_comments?)
     expect(TestClass).to receive(:block_was_called)
+    expect_any_instance_of(TestClass).to receive(:execute)
+    allow_any_instance_of(TestClass).to receive_message_chain(:schema_creation, :accept)
+    allow_any_instance_of(TestClass).to receive(:supports_indexes_in_create?)
+    allow_any_instance_of(TestClass).to receive(:supports_comments?)
   end
 
   describe '#create_table' do
+    # any_args is used to bridge differences between ActiveRecord versions
+
     context 'with create_options' do
       before(:each) do
         allow(ActiveRecord::Base).to receive(:connection_config).and_return({ create_options: create_options })
@@ -32,7 +34,7 @@ RSpec.describe ActiveRecord::ConnectionAdapters::SchemaStatementsExt do
       context 'with current_options' do
         it 'adds connection config to options arg and forwards to parent' do
           expect_any_instance_of(TestClass).to receive(:create_table_definition).with(
-            table, nil, both_options, nil, { comment: nil }).and_call_original
+            table, nil, both_options, any_args).and_call_original
 
           TestClass.new.create_table(table, { options: current_options }) { block }
         end
@@ -41,7 +43,7 @@ RSpec.describe ActiveRecord::ConnectionAdapters::SchemaStatementsExt do
       context 'without current_options' do
         it 'sends connection config as option' do
           expect_any_instance_of(TestClass).to receive(:create_table_definition).with(
-            table, nil, create_options, nil, { comment: nil }).and_call_original
+            table, nil, create_options, any_args).and_call_original
 
           TestClass.new.create_table(table) { block }
         end
@@ -56,7 +58,7 @@ RSpec.describe ActiveRecord::ConnectionAdapters::SchemaStatementsExt do
       context 'with current_options' do
         it 'sends just current_options' do
           expect_any_instance_of(TestClass).to receive(:create_table_definition).with(
-            table, nil, current_options, nil, { comment: nil }).and_call_original
+            table, nil, current_options, any_args).and_call_original
 
           TestClass.new.create_table(table, { options: current_options }) { block }
         end
@@ -65,7 +67,7 @@ RSpec.describe ActiveRecord::ConnectionAdapters::SchemaStatementsExt do
       context 'without current_options' do
         it 'sends no options' do
           expect_any_instance_of(TestClass).to receive(:create_table_definition).with(
-            table, nil, nil, nil, { comment: nil }).and_call_original
+            table, nil, nil, any_args).and_call_original
 
           TestClass.new.create_table(table) { block }
         end
